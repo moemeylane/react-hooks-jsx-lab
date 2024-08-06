8b9ca016108da635d7c482977537ff99380d0f30
moemeylane <wangarimaureen668@gmail.com>
2024-08-06 22:17:39 +0300

    initial commit

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>I'm doing the jsx lab</p>
+  <img src={image} alt="I made This"></img>
+  </div>;
 }
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,12 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
+
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div id="home"><h1 style={{color:"firebrick"}}>{name} ia a web Developer from {city}</h1></div>;
+
 }
+
+
 
 export default Home;
