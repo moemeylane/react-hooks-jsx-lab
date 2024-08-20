362430d26b0fb3281da6d0a14987d996570c2b1e
moemeylane <wangarimaureen668@gmail.com>
2024-08-20 07:09:10 +0300

    corrections

--- src/components/About.js
+++ src/components/About.js
@@ -2,12 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return (<div id="about">
-    <h2>About Me</h2>
-    <p>I'm doing the jsx lab</p>
-  <img src={ image } alt="I made This" />
-  </div>
-  )
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I'm doing the JSX lab</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,11 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
-
-  // update the JSX being returned!
-  return (<div id="home"><h1 style={{color:"firebrick"}}>{name} ia a web Developer from {city}</h1></div>
-  )
+  return (
+    <div id="home">
+      <h1 style={{ color: "firebrick" }}>{name} is a Web Developer from {city}</h1>
+    </div>
+  );
 }
-
-
 
 export default Home;
